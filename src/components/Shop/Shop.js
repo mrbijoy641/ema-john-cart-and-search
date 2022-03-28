38f0290b623332a6d4c50983e959b8b431ecca52
mrbijoy641 <mdbijoy641@gmail.com>
2022-03-28 19:20:25 +0600

    local-storage-create

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import '../../utilities/fakedb'
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -6,6 +8,7 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    // console.log(products);
 
     useEffect(() => {
         fetch('products.json')
@@ -13,11 +16,37 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+
+    // useEffect(() => {
+    //     const storedCart = getStoredCart();
+    //     // console.log(storedCart);
+    //     for (const id in storedCart) {
+    //         // console.log(id);
+    //         const addedProduct = products.find(product => product.id === id)
+    //         console.log(addedProduct)
+    //     }
+    // }, [])
+    useEffect(() => {
+        const storedCart = getStoredCart()
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            setCart(savedCart);
+        }
+    }, [products])
+
     const handleAddToCart = (product) => {
-        console.log(product);
+        // console.log(product);
         // do not do this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+
+        addToDb(product.id);
     }
 
     return (
